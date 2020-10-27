5d891a7a1152a73745e2c3563ccef699d8732d49
ajm <ajm>
2020-10-27 14:28:30 -0400

    An incomplete implementation of the python classes that would handle reaction coefficients and iterations, I'm going to archive this for now as I try something else

---
+++ CRIMSONSolver/ScalarProblem/SolverIteration.py
@@ -0,0 +1,26 @@
+#---------------------------------------------------------------------------------------------------
+## Class definition
+## Put this section in PythonModules\CRIMSONSolver\ScalarProblem\SolverIteration.py
+#---------------------------------------------------------------------------------------------------
+
+# source automatically generated for Python class "SolverIteration"
+# using GenerateNewSolverObject.
+#
+# Generated on 2020-10-23 14:42:43.123000
+
+from CRIMSONCore.PropertyStorage import PropertyStorage
+
+'''
+    TODO: Please put a useful comment here about what this class is
+'''
+class SolverIteration(PropertyStorage):
+
+    def __init__(self):
+        PropertyStorage.__init__(self)
+        self.properties = [
+            {
+                # TODO: Put your properties here
+            }
+        ]
+    
+    # TODO: Put any other methods you need here
